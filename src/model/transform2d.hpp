7aa59e9fc2c3d9afc00a450a0e23cf971288c066
Sawyer Stanley <sawyerscoding@gmail.com>
2024-12-11 10:32:38 -0400

    Created simple transform for entities

---
+++ src/model/transform2d.hpp
@@ -0,0 +1,27 @@
+#ifndef TRANSFORM2D_HPP
+#define TRANSFORM2D_HPP
+
+struct transform2D{
+
+	public:
+	const static float X_DEFAULT = 0.0f;
+	const static float Y_DEFAULT = 0.0f;
+	const static float ZROT_DEG_DEFAULT = 0.0f;
+	const static float WIDTH_DEFAULT = 1.0f;
+	const static float HEIGHT_DEFAULT = 1.0f;
+	const static float XVELOCITY_DEFAULT = 0.0f;
+	const static float YVELOCITY_DEFAULT = 0.0f;
+
+	float x;
+	float y;
+	float zrot_deg;
+	float width;
+	float height;
+	float xvelocity;
+	float yvelocity;
+	
+	transform2D(float x = X_DEFAULT, float y = Y_DEFAULT, float zrot_deg = ZROT_DEG_DEFAULT, float width = WIDTH_DEFAULT, float height = HEIGHT_DEFAULT, float xvelocity = XVELOCITY_DEFAULT, float yvelocity = YVELOCITY_DEFAULT) : x(x), y(y), zrot_deg(zrot_deg), width(width), height(height), xvelocity(xvelocity), yvelocity(yvelocity) {}
+	
+};
+
+#endif // TRANSFORM2D_HPP
